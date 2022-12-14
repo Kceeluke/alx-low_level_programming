0328d9f08351b0625fa4e3e0bd6fb5fdb2a2306c
kceeluke <collinsluke721@gmail.com>
2022-12-13 23:29:04 -0800

    p_putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#ifndef main_h
+#define main_h
 
 int _putchar(char);
 void print_alphabet(void);
@@ -12,3 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_tables(int n);
+
+#endif
