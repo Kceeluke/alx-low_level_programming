954a4cb7344e396ae6b6cb101f246a62a562a311
kceeluke <collinsluke721@gmail.com>
2023-01-06 14:05:07 -0800

    1-args.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,10 +5,9 @@
  * If you rename the program, it will print the new name, without having to
  * compile it again
  * You should not remove the path before the name of the program
- *
  * @argc: This is the argument count
  * @argv: This is the argumenti vector
-
+ *
  * Return: This return to 0
  */
 int main(int argc, char *argv[])
